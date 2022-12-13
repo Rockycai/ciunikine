e80ebc62d986f180cc3327467c6db4442f1b0d4d
rocky_cai <rocky_cai@ehsy.com>
2022-12-13 15:53:31 +0800

    ssh

--- server/app/server.go
+++ server/app/server.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	"ciunikine/api"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func setupRoutes() *echo.Echo {
@@ -12,6 +14,16 @@ func setupRoutes() *echo.Echo {
 	e.HideBanner = true
 
 	e.GET("/branding", api.Brand)
+
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
+
+	e.Use(middleware.Gzip())
 
 	return e
 }
